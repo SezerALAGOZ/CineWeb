6ef0fa02a2cb9b0acdb478d12e86f72cc45bae55
SezerALAGOZ <49189088+SezerALAGOZ@users.noreply.github.com>
2020-02-04 00:39:20 +0300

    Proje dosyası ekle.

---
+++ CineWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CineWeb.MvcApplication" Language="C#" %>
---
+++ CineWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CineWeb.Startup))]
+namespace CineWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
